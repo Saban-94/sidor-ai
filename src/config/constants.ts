9f2f18e9ea4369073345313458f0f3c6bf02179b
Saban-94 <hsaban2025@gmail.com>
2026-05-03 04:29:17 +0300

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,1 @@
-export const GAS_URL = import.meta.env.VITE_GAS_URL_GEMINI || import.meta.env.VITE_GAS_URL || "https://script.google.com/macros/s/AKfycbzhEuTe-PZpjD0lL5GziypNd-ZOged2XqWvJ4RFu9GvpImk3-YyorpbQGuIGipLTYts_Q/exec";
+export const GAS_URL = import.meta.env.VITE_GAS_URL_GEMINI || import.meta.env.VITE_GAS_URL || "https://script.google.com/macros/s/AKfycbw5mNSESiehR6J-0IGFrd7-AfJPRyPVWmpqzg-eKSr3vwnLTUbiSlYd3pO15pNd_FMM9Q/exec";
